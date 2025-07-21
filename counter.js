3c5bc7bdb8d1e4cae22f821dc27bfb4bed8fe46f
bipsashakya <bipsashakya3@gmail.com>
2025-07-21 13:24:56 +0545

    bbbb

---
+++ counter.js
@@ -0,0 +1,9 @@
+
+var count =0
+    function changeCount(){
+
+    }
+    function resetCount(){
+        let countElem=document.getElementById("count")
+        countElem.innerText=0
+    }
---
+++ test2.js
@@ -0,0 +1,84 @@
+
+//const prompt = require("prompt-sync")({sigint : true});
+
+// var college_name="lumbini"
+
+// if(true){
+//     var college_name="lumbini"
+//     console.log(college_name);
+
+// } if(true){
+//     let college_name="city"
+//     console.log(college_name);
+//     if(true){
+//     var college_name="college"
+//     console.log(college_name);
+// }
+// }
+// console.log(college_name);
+
+//  let person =["Bipsa","Arati","Nisa","Epsi"];
+// console.log(type of person);
+// Accessing method
+// console.log(person.at(-1));
+// console.log(person[1]);
+// push -- last element add
+
+// console.log(person.push("Feroz"));
+// console.log(person);
+
+
+//pop method
+// let person =["Bipsa","Arati","Nisa","Epsi"];
+// console.log(person.pop());
+
+// console.log(person);
+
+
+//update
+// let person =["Bipsa","Arati","Nisa","Epsi"];
+// //person.at()
+
+// person[1]="chill"
+// console.log(person);
+
+
+
+
+
+//to do list
+//  let todo = ["coding"];
+//  let person =["Bipsa","Arati","Nisa","Epsi"];
+//  for (let i of person){
+// console.log(i)
+//  }
+// task = prompt 
+//  person.push()
+
+
+// let todos=[];
+// while(true){
+//     console.log("1.add 2.view 3. edit 4. remove last");
+//     let choice =prompt("enter your choice");
+//     if(choice === "1"){
+//         let task = prompt("enter task:");
+//         todos.push(task);
+//         console.log("task added", task);
+//     }
+//     else if( choice === "2"){
+//         console.log(todos);
+//     }
+//     else if(choice === "3"){
+//         let index = prompt("enter task number to edit:");
+//         let values = prompt(" enter task number to edit");
+//         todos[index]= values;
+//     }
+//     else if( choice === "4"){
+//         todos.pop();
+//     }
+// }
+
+
+let array =["eps","nisa","rti"]
+
+console.log(array.indexOf("Bipsa"))
